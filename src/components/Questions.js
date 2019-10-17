0170f18c4cf7a22b1ef22b841c55aad9ec267f46
BebopVinh <vinhdo07@gmail.com>
2019-10-17 13:38:56 -0700

    Autoformatting

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,53 +1,46 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { Message, Button } from 'semantic-ui-react';
+import React from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+import { Message, Button } from "semantic-ui-react"
 
-const Questions = (props) => {
-  let answerChoices = []
-  let correctAnswer = ''
+const Questions = props => {
+	let answerChoices = []
+	let correctAnswer = ""
 
-  const handleClick = (e, { value }) => {
-    value === correctAnswer ? props.nextQuestion() : props.endGame()
-  }
+	const handleClick = (e, { value }) => {
+		value === correctAnswer ? props.nextQuestion() : props.endGame()
+	}
 
-    if (props.question) {
-      let rand
-      correctAnswer = props.question.correct_answer
-      console.log(correctAnswer)
-      answerChoices = [...props.question.incorrect_answers]
-      if (props.question.incorrect_answers.length == 1) {
-        rand = Math.floor(Math.random() * 2);
-        answerChoices.splice(rand, 0, correctAnswer)
-      } else {
-       rand = Math.floor(Math.random() * 4);
+	if (props.question) {
+		let rand
+		correctAnswer = props.question.correct_answer
+		console.log(correctAnswer)
+		answerChoices = [...props.question.incorrect_answers]
+		if (props.question.incorrect_answers.length == 1) {
+			rand = Math.floor(Math.random() * 2)
+			answerChoices.splice(rand, 0, correctAnswer)
+		} else {
+			rand = Math.floor(Math.random() * 4)
 
-       answerChoices.splice(rand, 0, correctAnswer)
-     }
-   }
-      return (
-        props.question ?
-          <div>
-            <h3>Category: { props.question.category }</h3>
-              <h2><strong>{ props.question.question }</strong></h2>
-                {answerChoices.map(choice => (
-                  <Button value={choice} onClick={handleClick}>{ choice }</Button>))}
-              </div>
-        : null
-      )
+			answerChoices.splice(rand, 0, correctAnswer)
+		}
+	}
+	return props.question ? (
+		<div>
+			<h3>Category: {props.question.category}</h3>
+			<h2>
+				<strong>{props.question.question}</strong>
+			</h2>
+			{answerChoices.map(choice => (
+				<Button value={choice} onClick={handleClick}>
+					{choice}
+				</Button>
+			))}
+		</div>
+	) : null
 }
 
-export default Questions;
-
-
-
-
-
-
-
-
-
-
+export default Questions
 
 // if props.question.correct_answer === userAnswer
 
